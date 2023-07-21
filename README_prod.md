0c62a0c153102132ebb6c0310719108035dfac52
sidazhou <sidazhou@gmail.com>
2023-07-21 20:33:14 +0800

    prod settings 3

--- README_prod.md
+++
@@ -1,6 +0,0 @@
-DEBUG=False
-ALLOWED_HOSTS
-STATIR_ROOT
-
-python manage.py collectstatic
-config nginx
--- prod_container_up.sh
+++ prod_container_up.sh
@@ -3,9 +3,30 @@
 docker rm -f djangomyhomepage &> /dev/null
 echo "starting container..."
 
+echo "collectstatic and moving to a public path that user www-data can access"
 docker run -it --rm \
         -v $(pwd):/usr/src/app \
         sidazhou/djangomyhomepage:latest python manage.py collectstatic
+
+# public static folder for nginx
+# tail -222 /var/log/nginx/error.log
+sudo mkdir -p /www-data/staticroot/
+sudo chown -R www-data:www-data /www-data/
+# https://superuser.com/a/1563652
+sudo find /www-data/ -type d -exec chmod 755 {} \;
+sudo find /www-data/ -type f -exec chmod 644 {} \;
+
+echo ''' nginx config should be:
+server {
+    listen 80;
+    server_name sidazhou.com;
+
+    # this is STATIC_URL in django
+    location /static/ {
+       alias /www-data/staticroot/;
+    }
+}
+'''
 
 docker run -itd \
                 -p 8000:8000 \
